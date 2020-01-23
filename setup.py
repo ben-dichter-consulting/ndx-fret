cb560edd3c8d4f7c053079c2fbdffab1729586ce
luiztauffer <luiz_tauffer@hotmail.com>
2020-01-23 15:54:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup_args = {
     'name': 'ndx-fret',
-    'version': '0.0.2',
+    'version': '0.1.0',
     'description': 'FRET',
     'long_description': long_description,
     'long_description_content_type': 'text/markdown',
