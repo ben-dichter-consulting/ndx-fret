ec0e552c85bdd3deb1fe80a06a8d5b27f4f03da0
luiz <luiz_tauffer@hotmail.com>
2020-11-25 14:07:58 +0100

    temporary solution to arg_type Device

--- src/pynwb/ndx_fret/__init__.py
+++ src/pynwb/ndx_fret/__init__.py
@@ -1,6 +1,8 @@
 import os
 from pynwb import load_namespaces, get_class
 from os import path
+import pynwb
+
 
 name = 'ndx-fret'
 
@@ -10,4 +12,7 @@ ns_path = os.path.join(here, 'spec', name + '.namespace.yaml')
 load_namespaces(ns_path)
 
 FRETSeries = get_class('FRETSeries', name)
+# Temporary solution
+FRETSeries.__init__.__docval__['args'][3]['type'] = pynwb.device.Device
+
 FRET = get_class('FRET', name)
